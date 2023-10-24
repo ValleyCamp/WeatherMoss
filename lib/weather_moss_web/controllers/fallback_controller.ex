92b86089f40bbb9b3f401ede3defb56b414abe4b
Josh Proehl <josh@daedalusdreams.com>
2023-10-24 17:38:31 -0500

    Add a fallback to FallbackController. Fallception.

--- lib/weather_moss_web/controllers/fallback_controller.ex
+++ lib/weather_moss_web/controllers/fallback_controller.ex
@@ -5,6 +5,7 @@ defmodule WeatherMossWeb.FallbackController do
   See `Phoenix.Controller.action_fallback/1` for more details.
   """
   use WeatherMossWeb, :controller
+  require Logger
 
   # This clause handles errors returned by Ecto's insert/update/delete.
   def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
@@ -20,6 +21,13 @@ defmodule WeatherMossWeb.FallbackController do
     |> put_status(:not_found)
     |> put_view(html: WeatherMossWeb.ErrorHTML, json: WeatherMossWeb.ErrorJSON)
     |> render(:"404")
+  end
+
+  def call(conn, err) do
+    Logger.error("FallbackController handling an unknown error!\n\tError was: #{inspect err}\n\tConn was:#{inspect conn}")
+    conn
+    |> put_status(:bad_request)
+    |> json(%{status: "error", error: "#{inspect err}"})
   end
 
 end
