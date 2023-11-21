7c5c57c63fac1821430ab9f966162e302e10c312
Josh Proehl <josh@daedalusdreams.com>
2023-11-21 16:43:34 -0600

    Add/refactor rain to fake_weather

--- priv/dev/fake_weather.ex
+++ priv/dev/fake_weather.ex
@@ -16,11 +16,15 @@ defmodule WeatherMoss.FakeWeather do
 #            ticks_since_cloudcover_change: nil,
 #            ticks_since_wind_event_started: nil,
 #            ticks_since_rain_event_started: nil,
+#            ticks_without_rain: nil,
 #            ticks_since_lightning_event_started: nil,
 #            cloudcover_pct: nil,
 #            base_illuminance: nil,
 #            base_temp_F: nil,
 #            illuminance: nil,
+#            rain_accumulated_mm: nil,
+#            rain_day_accumulated_mm: nil,
+#            rain_precip_type: nil,
 #            temp_F: nil,
 #            humidity_pct: nil,
 #            pressure_inHg: nil,
@@ -34,11 +38,15 @@ defmodule WeatherMoss.FakeWeather do
 #               ticks_since_cloudcover_change: integer(),
 #               ticks_since_wind_event_started: integer(),
 #               ticks_since_rain_event_started: integer(),
+#               ticks_without_rain: integer(),
 #               ticks_since_lightning_event_started: integer(),
 #               cloudcover_pct: integer(),
 #               base_illuminance: integer(),
 #               base_temp_F: float(),
 #               illuminance: integer(),
+#               rain_accumulated_mm: float(),
+#               rain_day_accumulated_mm: float(),
+#               rain_precip_type: term(), # atom, one of: :none, :rain, :hail, :rain_plus_hail
 #               temp_F: float(),
 #               humidity_pct: integer(),
 #               pressure_inHg: float(),
@@ -78,7 +86,6 @@ defmodule WeatherMoss.FakeWeather do
       |> Map.put(:fake_time, state.latest.fake_time |> Time.add(10, :minute))
       |> Map.put(:ticks_since_cloudcover_change, state.latest.ticks_since_cloudcover_change + 1)
       |> Map.put(:ticks_since_wind_event_started, state.latest.ticks_since_wind_event_started + 1)
-      |> Map.put(:ticks_since_rain_event_started, state.latest.ticks_since_rain_event_started + 1)
       |> Map.put(:ticks_since_lightning_event_started, state.latest.ticks_since_lightning_event_started + 1)
       |> do_tick_weather_changes()
 
@@ -117,7 +124,9 @@ defmodule WeatherMoss.FakeWeather do
     |> Map.put(:ticks_since_cloudcover_change, 0)
     |> Map.put(:ticks_since_wind_event_started, 0)
     |> Map.put(:ticks_since_rain_event_started, 0)
+    |> Map.put(:ticks_without_rain, 0)
     |> Map.put(:ticks_since_lightning_event_started, 0)
+    |> Map.put(:rain_day_accumulated_mm, Enum.random(1..10))
     |> do_tick_weather_changes()
   end
 
@@ -146,81 +155,82 @@ defmodule WeatherMoss.FakeWeather do
   # Functions for changing the "base" values that represent the state of the world and will impact
   # the actual calculated values.
 
-  defp reset_day_if_needed(state) do
+  defp reset_day_if_needed(latest) do
     # A new day means we need to calculate the new min/max values for the day, which our later values
     # are going to depend on.
-    if state.fake_time.hour == 0 and state.fake_time.minute == 0 do
+    if latest.fake_time.hour == 0 and latest.fake_time.minute == 0 do
       new_fake_day =
-        state.fake_today
+        latest.fake_today
         |> Map.put(:peak_illuminance, Enum.random(20_000..160_000))
         |> (fn s -> %{s| min_temp_F: s.min_temp_F + (Enum.random(5..15) * Enum.random([-1, 1]))} end).()
         |> (fn s -> %{s| max_temp_F: s.min_temp_F + Enum.random(5..30)} end).()
         |> Map.put(:pressure_direction, Enum.random([-1, 0, 1]))
 
-      state
+      latest
       |> Map.put(:fake_today, new_fake_day)
       |> Map.put(:base_humidity_pct, Enum.random(50..60))
+      |> Map.put(:rain_day_accumulated_mm, 0)
     else
-      state
+      latest
     end
   end
 
-  defp alter_base_illuminance_for_time(state) do
+  defp alter_base_illuminance_for_time(latest) do
     # Builds the current base illuminance. If state.fake_time is before state.fake_today.sunrise, then it will be 0,
     # after which it will grow steadily to the state.fake_today.peak_illuminance value at state.fake_today.solar_noon,
     # and then fall steadily back to zero at state.fake_today.sunset.
-    base_illuminance = case state.fake_time do
-      time when time < state.fake_today.sunrise -> 0
-      time when time < state.fake_today.solar_noon -> (state.fake_today.peak_illuminance / (Time.diff(state.fake_today.solar_noon, state.fake_today.sunrise, :minute)/10)) * (Time.diff(state.fake_today.sunrise, time, :minute)/10) # ((peak illuminance at noon) / (number of steps between sunrise and noon)) * (Number of steps since sunrise)
-      time when time < state.fake_today.sunset -> (state.fake_today.peak_illuminance / (Time.diff(state.fake_today.sunset, state.fake_today.solar_noon, :minute)/10)) * (Time.diff(state.fake_today.solar_noon, time, :minute)/10) # ((peak illuminance at noon) / (number of steps between noon and sunset)) * (Number of steps since noon)
+    base_illuminance = case latest.fake_time do
+      time when time < latest.fake_today.sunrise -> 0
+      time when time < latest.fake_today.solar_noon -> (latest.fake_today.peak_illuminance / (Time.diff(latest.fake_today.solar_noon, latest.fake_today.sunrise, :minute)/10)) * (Time.diff(latest.fake_today.sunrise, time, :minute)/10) # ((peak illuminance at noon) / (number of steps between sunrise and noon)) * (Number of steps since sunrise)
+      time when time < latest.fake_today.sunset -> (latest.fake_today.peak_illuminance / (Time.diff(latest.fake_today.sunset, latest.fake_today.solar_noon, :minute)/10)) * (Time.diff(latest.fake_today.solar_noon, time, :minute)/10) # ((peak illuminance at noon) / (number of steps between noon and sunset)) * (Number of steps since noon)
       _ -> 0
     end
 
     # Finally, we'll add the illuminance to the state and return the modified state.
-    state
+    latest
     |> Map.put(:base_illuminance, base_illuminance)
   end
 
-  defp alter_base_temp_F_for_time(state) do
+  defp alter_base_temp_F_for_time(latest) do
     # Calculate a base_temp_F.
     # If the current state.fake_time is before state.fake_today.sunrise, then the temp should continue dropping to, but
     # not past, state.fake_today.min_temp_F.
     # After state.fake_today.sunrise the temp should rise consistently until it reaches state.fake_today.max_temp_F at
     # 2 hours before state.fake_today.sunset, after which in should fall consistently until sunrise.
-    peak_temp_time = state.fake_today.sunset |> Time.add(-2, :hour)
-    cur = Map.get(state, :base_temp_F, Enum.random(state.fake_today.min_temp_F..state.fake_today.max_temp_F))
-    diff_per_tick = case state.fake_time do
-      time when time < state.fake_today.sunrise ->
-        ticks_to_sunrise = Time.diff(state.fake_today.sunrise, state.fake_time, :minute)/10
-        (cur - state.fake_today.min_temp_F) / ticks_to_sunrise
+    peak_temp_time = latest.fake_today.sunset |> Time.add(-2, :hour)
+    cur = Map.get(latest, :base_temp_F, Enum.random(latest.fake_today.min_temp_F..latest.fake_today.max_temp_F))
+    diff_per_tick = case latest.fake_time do
+      time when time < latest.fake_today.sunrise ->
+        ticks_to_sunrise = Time.diff(latest.fake_today.sunrise, latest.fake_time, :minute)/10
+        (cur - latest.fake_today.min_temp_F) / ticks_to_sunrise
       time when time < peak_temp_time ->
-        ticks_to_peak = Time.diff(peak_temp_time, state.fake_time, :minute)/10
-        (cur + state.fake_today.max_temp_F) / ticks_to_peak
+        ticks_to_peak = Time.diff(peak_temp_time, latest.fake_time, :minute)/10
+        (cur + latest.fake_today.max_temp_F) / ticks_to_peak
       time when time > peak_temp_time ->
-        ticks_to_midnight = Time.diff(Time.new!(0,0,0,0), state.fake_time, :minute)/10
-        (cur - state.fake_today.min_temp_F) / ticks_to_midnight
+        ticks_to_midnight = Time.diff(Time.new!(0,0,0,0), latest.fake_time, :minute)/10
+        (cur - latest.fake_today.min_temp_F) / ticks_to_midnight
       _ ->
         Logger.error("altering base_temp_F hit a snag. This was the fault of the programmer.")
         0
     end
 
-    state
+    latest
     |> Map.put(:base_temp_F, cur + diff_per_tick)
   end
 
-  defp alter_base_humidity_pct(state) do
-    initial = Map.get(state, :base_humidity_pct, Enum.random(50..60))
+  defp alter_base_humidity_pct(latest) do
+    initial = Map.get(latest, :base_humidity_pct, Enum.random(50..60))
 
     #TODO: Randomize change here, based on.... ?
 
-    state
+    latest
     |> Map.put(:base_humidity_pct, initial)
   end
 
-  defp alter_cloudcover(state) do
+  defp alter_cloudcover(latest) do
     r = %{
-      cloudcover_pct: Map.get(state, :cloudcover_pct, Enum.random(0..100)),
-      ticks_since_cloudcover_change: state.ticks_since_cloudcover_change
+      cloudcover_pct: Map.get(latest, :cloudcover_pct, Enum.random(0..100)),
+      ticks_since_cloudcover_change: latest.ticks_since_cloudcover_change
     }
     randomly_change_after = Enum.random(10..20)
 
@@ -237,37 +247,96 @@ defmodule WeatherMoss.FakeWeather do
         }
     end
 
-    Map.merge(state, r)
+    Map.merge(latest, r)
   end
 
 
   #####
   # These functions are the ones that actually calculate the "true" values for the current weather.
 
-  defp calc_illuminance(state) do
+  defp calc_illuminance(latest) do
     # Calculate the true current illuminance value, based on the base_illuminance and the cloudcover_pct.
     # (If the cloudcover is 100, the illuminance should be reduced by half.)
-    state
-    |> Map.put(:illuminance, state.base_illuminance * ((1 - (state.cloudcover_pct / 100)) * 0.5))
+    latest
+    |> Map.put(:illuminance, latest.base_illuminance * ((1 - (latest.cloudcover_pct / 100)) * 0.5))
   end
 
-  defp calc_uv_index(state) do
-    state
-    |> Map.put(:uv_index, uv_from_illuminance(state.illuminance))
+  defp calc_uv_index(latest) do
+    latest
+    |> Map.put(:uv_index, uv_from_illuminance(latest.illuminance))
   end
 
-  defp calc_wm2(state) do
-    state
-    |> Map.put(:wm2, wm2_from_illuminance(state.illuminance))
+  defp calc_wm2(latest) do
+    latest
+    |> Map.put(:wm2, wm2_from_illuminance(latest.illuminance))
   end
 
-  defp calc_rain(state) do
-    state
+  defp calc_rain(latest) do
+    # This represents the amount of rain that has been accumulated since the last tick.
+    # Rain is going to depend on a few things:
+    # 1) Cloudcover PCT.
+    #   A) If < 25, no rain possible.
+    #   B) If < 50, rain only up to a certain rate possible
+    # 2) Is it currently raining? 
+    # 3) Number of ticks since last rain event
+    # 4) Temperature. Is it hailing, or snowing?
+    #     NOTE: Neither weather station specifically detects "snow"
+
+    # Wikipedia: 
+    # Light rain describes rainfall which falls at a rate of between
+    # a trace and 2.5 millimetres (0.098 in) per hour.
+    # Moderate rain describes rainfall with a precipitation rate of between
+    # 2.6 millimetres (0.10 in) and 7.6 millimetres (0.30 in) per hour.
+    # Heavy rain describes rainfall with a precipitation rate above
+    # 7.6 millimetres (0.30 in) per hour, and violent rain has a
+    # rate more than 50 millimetres (2.0 in) per hour.[11]
+
+    # Max rain rate is dictated my current cloudcover
+    # This is expressed in millimeters per hour, which is NOT the
+    # correct unit of measurement per-tick!
+    max_rain_rate = case latest.cloudcover_pct do
+      cc when cc > 98 -> 30.0
+      cc when cc > 75 -> 7.6
+      cc when cc > 50 -> 2.6
+      cc when cc > 25 -> 0.5
+      _ -> 0.0
+    end
+
+    prev_accumulation = Map.get(latest, :rain_accumulated_mm, 0)
+    prev_precip_type = Map.get(latest, :rain_precip_type, :none)
+
+    # For the sake of being able to test data we're going to make sure that there's never a dry spell
+    # or a rain event that lasts too long. We to change states as often as possible, but not so often
+    # that we don't also get a chance to see the data changing.
+    {accumulation, precip_type, ticks_without_rain, ticks_since_rain_event_started} = cond do
+      latest.ticks_without_rain > 12 -> 
+        # We've been dry too long, start at a value in the upper half of the current rain rate.
+        # TODO: Make it hail occasionally?
+        min_rain_rate = max_rain_rate / 2
+        new_rain = add_random_with_limit(prev_accumulation, {min_rain_rate, max_rain_rate, 1}, {0, max_rain_rate})
+        {new_rain, :rain, latest.ticks_without_rain + 1, 0}
+      latest.ticks_since_rain_event_started > 12 ->
+        # It's been raining too long, let's stop.
+        {0, :none, 0, latest.ticks_since_rain_event_started + 1}
+      prev_accumulation > 0 ->
+        # No major change, so we make a minor modification to the existing state
+        {add_random_with_limit(prev_accumulation, {-0.2, 0.2, 1}, {0, max_rain_rate}), prev_precip_type, 0, latest.ticks_since_rain_event_started + 1}
+      true -> 
+        # This implies it's not currently raining, and no changes are needed
+        {0, :none, 0, latest.ticks_since_rain_event_started + 1}
+    end
+
+    latest
+    |> Map.put(:ticks_since_rain_event_started, ticks_since_rain_event_started)
+    |> Map.put(:ticks_without_rain, ticks_without_rain)
+    |> Map.put(:rain_accumulated_mm, accumulation)
+    |> Map.put(:rain_day_accumulated_mm, Map.get(latest, :rain_day_accumulated_mm) + accumulation)
+    |> Map.put(:rain_precip_type, precip_type)
   end
 
-  defp calc_temp_F(state) do
+  defp calc_temp_F(latest) do
     # Made up non-scientific values indicating how much cloudcover impacts temperature.
-    cloudcover_temp_F_modifier = case state.cloudcover_pct do
+    cloudcover_temp_F_modifier = case latest.cloudcover_pct do
       c when c > 90 -> 0.5
       c when c > 75 -> 0.4
       c when c > 50 -> 0.25
@@ -275,24 +344,24 @@ defmodule WeatherMoss.FakeWeather do
       _c -> 0
     end
 
-    state
-    |> Map.put(:temp_F, state.base_temp_F * cloudcover_temp_F_modifier)
+    latest
+    |> Map.put(:temp_F, latest.base_temp_F * cloudcover_temp_F_modifier)
   end
 
-  defp calc_humidity_pct(state) do
-    initial = Map.get(state, :base_humidity_pct, Enum.random(50..60))
-    cloudcover_contribution = Float.round(state.cloudcover_pct * 0.2, 2)
+  defp calc_humidity_pct(latest) do
+    initial = Map.get(latest, :base_humidity_pct, Enum.random(50..60))
+    cloudcover_contribution = Float.round(latest.cloudcover_pct * 0.2, 2)
     # TODO: rain_contribution = ...
 
-    state
+    latest
     |> Map.put(:humidity_pct, initial + cloudcover_contribution)
   end
 
-  defp calc_pressure_inHg(state) do
-    initial = Map.get(state, :pressure_inHg, 30.0)
+  defp calc_pressure_inHg(latest) do
+    initial = Map.get(latest, :pressure_inHg, 30.0)
 
-    state
-    |> Map.put(:pressure_inHg, initial + ((Enum.random(0..10) * 0.01) * state.fake_today.pressure_dir))
+    latest
+    |> Map.put(:pressure_inHg, initial + ((Enum.random(0..10) * 0.01) * latest.fake_today.pressure_dir))
   end
 
 
@@ -315,17 +384,23 @@ defmodule WeatherMoss.FakeWeather do
     rand_val = Enum.random(random_range)
     add_random_with_limit(base, rand_val, limit_range)
   end
-  # This version accepts an input tuple that allows us to define a range, generate a float within that range, and round to given decimal precision.
+  # These versions accepts an input tuple that allows us to define a range, generate a float within that range, and round to given decimal precision.
   defp add_random_with_limit(base, {rand_start, rand_end, decimal_places}, %Range{} = limit_range) do
+    add_random_with_limit(base, {rand_start, rand_end, decimal_places}, {limit_range.first, limit_range.last})
+  end
+  defp add_random_with_limit(base, {rand_start, rand_end, decimal_places}, {low_limit, high_limit}) do # I'd rather have a variable limit_tuple here, but then the pattern match isn't as restrictive, so...
     rand_val = Float.round(:rand.uniform() * (rand_end - rand_start) + rand_start, decimal_places)
-    add_random_with_limit(base, rand_val, limit_range)
+    add_random_with_limit(base, rand_val, {low_limit, high_limit})
   end
   # Note that this is the final fall-through of the function, and does not actually do anything random, just adds and checks.
   defp add_random_with_limit(base, chosen_random_value, %Range{} = limit_range) when is_number(chosen_random_value) do
+    add_random_with_limit(base, chosen_random_value, {limit_range.first, limit_range.last})
+  end
+  defp add_random_with_limit(base, chosen_random_value, {low_limit, high_limit}) when is_number(chosen_random_value) do
     res = base + chosen_random_value
     case res do
-      val when val < limit_range.first -> limit_range.first
-      val when val > limit_range.last -> limit_range.last
+      val when val < low_limit -> low_limit
+      val when val > high_limit -> high_limit
       val -> val
     end
   end
